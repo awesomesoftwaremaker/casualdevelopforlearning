372c083e5a51256ef5fbfcf499d252066b6c5066
awesomesoftwaremaker <36794071+awesomesoftwaremaker@users.noreply.github.com>
2018-02-26 20:36:57 +0600

    1.st homework
    access to massive via three different methods

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define MDA multidimensional_array
+#define COL 3 //quantity of columns
+
+const int multidimensional_array [][COL] = {{1, 2, 3}, {4, 5, 6}};
+
+int indexing (int x, int y)
+{
+
+}
+
+int increment (int x, int y)
+{
+
+}
+
+int pointer (int x, int y)
+{
+
+}
+
+int main ()
+{
+    printf("Задан двумерный массив, состоящий из 2-х строк и 3-х столбцоа.\n"
+           "Введите координату X и Y требуемого элемента массива\n");
+
+    int x,y;
+    scanf("%d%d", x, y);
+
+}
