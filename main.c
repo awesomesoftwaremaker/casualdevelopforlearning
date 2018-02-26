57efb30ede7124edb46e2199c38011fafd5ac880
awesomesoftwaremaker <36794071+awesomesoftwaremaker@users.noreply.github.com>
2018-02-26 21:51:31 +0600

    Update main.c

--- main.c
+++ main.c
@@ -4,18 +4,19 @@
 #define COL 3 //quantity of columns
 
 const int multidimensional_array [][COL] = {{54, 32, 77}, {21, 10, 64}};
+int x,y;
 
-int indexing (x,y)
+int indexing ()
 {
     return MDA [x][y];
 }
 
-int increment (x,y)
+int increment ()
 {
     return *(*(MDA + x) + y);
 }
 
-int pointer (x,y)
+int pointer ()
 {
     int value = (x * COL) + y;
     return *(*MDA + value);
@@ -24,8 +25,6 @@ int pointer (x,y)
 int main ()
 {
     printf("enter x and y\n");
-
-    int x,y;
     scanf("%d%d", x, y);
     printf("%d%d%d", indexing, increment, pointer);
     getchar();
